b438b385d08c4a49221175d1e56fc324e09902bb
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-06-07 17:30:01 -0300

    learning: Modules, services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // Recebe os controllers que existem nesses modules.
+  providers: [AppService], // Declarar todas as dependências que os meus controllers podem ter.
 })
+
+/*
+- Tudo que não é controller, provavelmente será um provider
+- Tudo que não recebo requisição HTTP, é um provider
+- Tudo que eu vou precisar injetar em outras classes, é um provider
+*/
+
+// Controllers => tudo que recebe requisição HTTP
 export class AppModule {}
