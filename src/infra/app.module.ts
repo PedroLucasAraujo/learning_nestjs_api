49357b94aee44af87134b22f495dfcf17c3dbd14
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-07-12 18:01:04 -0300

    feat: implementandos repositórios do prisma

---
+++ src/infra/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+import { HttpModule } from "./http/http.module";
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+  ],
+})
+
+/*
+- Tudo que não é controller, provavelmente será um provider
+- Tudo que não recebo requisição HTTP, é um provider
+- Tudo que eu vou precisar injetar em outras classes, é um provider
+*/
+
+// Controllers => tudo que recebe requisição HTTP
+export class AppModule {}
--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -5,21 +5,21 @@ import {
   Controller,
   HttpCode,
   Post,
-} from '@nestjs/common'
+} from "@nestjs/common";
 
-import { PrismaService } from '@/prisma/prisma.service'
-import { hash } from 'bcryptjs'
-import { z } from 'zod'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { hash } from "bcryptjs";
+import { z } from "zod";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 
 const createAccountBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string(),
-})
+});
 
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
-@Controller('/accounts')
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+@Controller("/accounts")
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
@@ -27,21 +27,21 @@ export class CreateAccountController {
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
-    const { name, email, password } = body
+    const { name, email, password } = body;
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
         email,
       },
-    })
+    });
 
     if (userWithSameEmail) {
       throw new ConflictException(
-        'Já existe um usuário cadastrado com esse e-mail.',
-      )
+        "Já existe um usuário cadastrado com esse e-mail."
+      );
     }
 
-    const hashedPassword = await hash(password, 8)
+    const hashedPassword = await hash(password, 8);
 
     await this.prisma.user.create({
       data: {
@@ -49,6 +49,6 @@ export class CreateAccountController {
         email,
         password: hashedPassword,
       },
-    })
+    });
   }
 }
