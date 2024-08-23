6eacf18f757994253d7d543ec7174e441f5c459f
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-08-23 14:36:37 -0300

    feat: registering domain events

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env/env'
-import { AuthModule } from './auth/auth.module'
-import { HttpModule } from './http/http.module'
-import { EnvService } from './env/env.service'
-import { EnvModule } from './env/env.module'
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env/env";
+import { AuthModule } from "./auth/auth.module";
+import { HttpModule } from "./http/http.module";
+import { EnvService } from "./env/env.service";
+import { EnvModule } from "./env/env.module";
+import { EventsModule } from "./events/events.module";
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,6 +15,7 @@ import { EnvModule } from './env/env.module'
     AuthModule,
     HttpModule,
     EnvModule,
+    EventsModule,
   ],
   providers: [EnvService],
 })
--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,21 +1,23 @@
-import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
-import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
-import { PrismaAnswersCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
-import { PrismaAnswerRepository } from './prisma/repositories/prisma-answer-repository'
-import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
-import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
-import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions-repository'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
-import { PrismaStudentRepository } from './prisma/repositories/prisma-students-repository'
-import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
-import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
-import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
-import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
-import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
-import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
-import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository'
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
+import { PrismaAnswersCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { PrismaAnswerRepository } from "./prisma/repositories/prisma-answer-repository";
+import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question-attachments-repository";
+import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments-repository";
+import { PrismaQuestionRepository } from "./prisma/repositories/prisma-questions-repository";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
+import { PrismaStudentRepository } from "./prisma/repositories/prisma-students-repository";
+import { StudentsRepository } from "@/domain/forum/application/repositories/students-repository";
+import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
+import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
+import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
+import { AttachmentsRepository } from "@/domain/forum/application/repositories/attachments-repository";
+import { PrismaAttachmentsRepository } from "./prisma/repositories/prisma-attachments-repository";
+import { NotificationsRepository } from "@/domain/notification/application/repositories/notifications-repository";
+import { PrismaNotificationsRepository } from "./prisma/repositories/prisma-notifications-repository";
 
 @Module({
   providers: [
@@ -39,6 +41,10 @@ import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attach
     {
       provide: QuestionCommentsRepository,
       useClass: PrismaQuestionCommentsRepository,
+    },
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
     },
     { provide: QuestionsRepository, useClass: PrismaQuestionRepository },
     { provide: StudentsRepository, useClass: PrismaStudentRepository },
